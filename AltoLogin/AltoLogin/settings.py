db64bbfb6544aeb4c3a1bf2e48b9175d6d82aedc
Abner Torres <abnergama2512@gmail.com>
2024-03-20 08:38:19 -0300

    ft: serializer funcionando

--- AltoLogin/AltoLogin/settings.py
+++ AltoLogin/AltoLogin/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuario.apps.UsuarioConfig',
-    
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- AltoLogin/usuario/urls.py
+++ AltoLogin/usuario/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework import routers
+from usuario.api import viewsets as userviewsets
+
+route = routers.DefaultRouter()
+route.register(r'cadastro', userviewsets.UsuarioViewSet, basename='Cadastro')
+
 
 urlpatterns = [
     path('', views.HomePage, name='HomePage'),
     path('login/', views.LoginPage, name='LoginPage'),
-    path('register/', views.RegisterPage, name='RegisterPage'),
+    #path('register/', views.RegisterPage, name='RegisterPage'),
+    path('', include(route.urls))
 ]
